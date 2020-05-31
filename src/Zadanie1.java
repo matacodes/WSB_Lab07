0afbf34066f35877291ed86e890acecef21418de
matacodes <ma.matysek@gmail.com>
2020-05-31 10:09:18 +0200

    TestExceptions.java

---
+++ src/Zadanie1.java
@@ -0,0 +1,15 @@
+
+public class Zadanie1 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		try {
+			for (int i = 0; true; i++) {
+				System.out.println("args[" + i + "] is '" + args[i] + "'");
+			}
+		} catch (ArrayIndexOutOfBoundsException e) {
+			System.err.println("Quiting...");
+		}
+	}
+}
